dd3ee64381e7cf567eba6d848d7e85b81a828b24
Matthieu <matthieusatan@hotmail.fr>
2020-01-03 19:31:59 +0100

    Working on climate analysis

--- app/extract.py
+++ app/extract.py
@@ -6,7 +6,7 @@ main_url = 'https://www.infoclimat.fr/climatologie-mensuelle/07156/{0}/{1}/paris
 df_data = pd.DataFrame(columns=['year', 'month', 'day', 't_min', 't_max', 'rain'])
 
 yr_start = 1886
-yr_stop = 2018
+yr_stop = 2019
 
 if __name__ == '__main__':
 
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     for yr in range(yr_start,yr_stop+1,1):
         print('Year:',yr)
         for mh in range(1,13,1):
-            
+
             df_data = df_data.append(utils.monthly_data(yr, mh, main_url))
 
         file_name = '../data/' + 'data_paris_montsouris.csv'
